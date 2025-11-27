c4214e2b75f360c2765ba66e207a761848c1f5f5
Руслан Каписъски <ruslank1@abv.bg>
2025-11-27 13:57:51 +0200

    Create authorization middleware

---
+++ api/src/middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../constants/constants";
+
+export function authMiddleware(req, res, next) {
+  const token = req.header("X-Authorization");
+
+  if (!token) {
+    return next();
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, JWT_SECRET);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      message: error.message,
+    });
+  }
+}
