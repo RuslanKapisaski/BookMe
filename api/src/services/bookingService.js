588fc0b9f7f5f68ad5b633d4cbbfb77000641f3f
Руслан Каписъски <ruslank1@abv.bg>
2025-11-28 14:22:37 +0200

    Create booking service

---
+++ api/src/services/bookingService.js
@@ -0,0 +1,43 @@
+import Booking from "../models/Booking.js";
+
+export default {
+  getById(bookingId) {
+    return Booking.findById(bookingId).populate([
+      "guest",
+      "property",
+      "dateFrom",
+      "dateTo",
+      "guestsCount",
+    ]);
+  },
+
+  getAll() {
+    return Booking.find();
+  },
+
+  getLatest() {
+    return Booking.find().sort({ _id: -1 });
+  },
+
+  getAllByUser(userId) {
+    return Booking.find({ guest: userId });
+  },
+
+  create(bookingData) {
+    return Booking.create(bookingData);
+  },
+
+  edit(bookingId, bookingData) {
+    return Booking.findByIdAndUpdate(bookingId, bookingData);
+  },
+
+  async remove(userId, bookingId) {
+    const booking = await Booking.findById(bookingId);
+
+    if (!booking.user.equals(userId)) {
+      throw new Error("Only creator of the booking can delete this booking!");
+    }
+
+    return await Booking.findByIdAndDelete(bookingId);
+  },
+};
