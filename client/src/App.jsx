1d6154faa9e40a1b2af39dc90aa2746be9dcba6b
Руслан Каписъски <ruslank1@abv.bg>
2025-12-05 16:05:00 +0200

    Show static header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,11 @@
 import { Routes, Route } from "react-router";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <h1 className="text-4xl font-bold text-blue-600 text-center mt-10">
-              Hello Tailwind 🚀
-            </h1>
-          }
-        />
-      </Routes>
+      <Header />
+      <Routes></Routes>
     </>
   );
 }
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router";
+import { useUserContext } from "../../contexts/UserContext";
+
+export default function Header() {
+  const { isAuthenticated, user } = useUserContext();
+
+  return (
+    <>
+      <header className="bg-sky-950 text-white shadow-md font-medium">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between h-16 items-center">
+            <div className="flex-shrink-0 text-2xl font-bold">BookMe</div>
+
+            <nav className="hidden md:flex space-x-4">
+              <Link to="/" className="hover:text-gray-200">
+                Home
+              </Link>
+              {isAuthenticated ? (
+                <div>
+                  <Link to="/about" className="hover:text-gray-200 p-2">
+                    Add property
+                  </Link>
+
+                  <Link to="/profile" className="hover:text-gray-200 p-2">
+                    Profile
+                  </Link>
+                </div>
+              ) : (
+                <div>
+                  <Link to="/about" className="hover:text-gray-200 p-2">
+                    About
+                  </Link>
+
+                  <Link to="/register" className="hover:text-gray-200 p-2">
+                    Register
+                  </Link>
+
+                  <Link to="/login" className="hover:text-gray-200 p-2">
+                    Login
+                  </Link>
+                </div>
+              )}
+              {user && <p>{user.email}</p>}
+            </nav>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
