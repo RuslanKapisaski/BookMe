059c601c5a8ba4a62bf5da31cf651162d90d7a18
Руслан Каписъски <ruslank1@abv.bg>
2025-12-05 16:22:25 +0200

    Add register component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Register from "./components/register/Register";
 
 function App() {
   return (
     <>
       <Header />
-      <Routes></Routes>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+      </Routes>
       <Footer />
     </>
   );
