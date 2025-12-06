9a818ed80c04dd51fb34fbae692bba66377cf20c
Руслан Каписъски <ruslank1@abv.bg>
2025-12-06 14:41:56 +0200

    Upgrade booking model
    Make booking calculation in data base layer, not in client where information is easily manipulated

--- api/src/models/Booking.js
+++ api/src/models/Booking.js
@@ -29,11 +29,38 @@ const bookingSchema = new Schema(
       required: [true, "Enter guests count!"],
       min: [0, "Invalid guests count!"],
     },
+    bill: {
+      type: Number,
+      required: [true, "Bill is required!"],
+    },
+    period: {
+      type: Number,
+      required: [true, "Period is required!"],
+      min: [1, "Period cannot be less than 1 day!"],
+    },
   },
   {
     timestamps: true,
   }
 );
+
+bookingSchema.pre("save", function (next) {
+  const periodInMillis = this.dateTo - this.dateFrom;
+  const periodInDays = Math.floor(periodInMillis / (1000 * 60 * 60 * 24));
+
+  if (periodInDays < 0) {
+    return next(new Error("End date must be after start date!"));
+  }
+
+  this.model("Property").findById(this.property, (err, property) => {
+    if (err || !property) {
+      return next(new Error("Property not found or error fetching property!"));
+    }
+    this.bill = periodInDays * property.pricePerNight;
+    this.period = daysDiffTime;
+    next();
+  });
+});
 
 const Booking = model("Booking", bookingSchema);
 
