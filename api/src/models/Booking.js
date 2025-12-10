9c6c3108c1c8182c7c645d98442758284bcd5308
Руслан Каписъски <ruslank1@abv.bg>
2025-12-10 11:19:46 +0200

    Booking model refactor

--- api/src/models/Booking.js
+++ api/src/models/Booking.js
@@ -27,7 +27,7 @@ const bookingSchema = new Schema(
     guestsCount: {
       type: Number,
       required: [true, "Enter guests count!"],
-      min: [0, "Invalid guests count!"],
+      min: [1, "Invalid guests count!"],
     },
     bill: {
       type: Number,
@@ -38,28 +38,30 @@ const bookingSchema = new Schema(
       required: [true, "Period is required!"],
       min: [1, "Period cannot be less than 1 day!"],
     },
+    note: {
+      type: String,
+    },
   },
   {
     timestamps: true,
   }
 );
 
-bookingSchema.pre("save", function (next) {
-  const periodInMillis = this.dateTo - this.dateFrom;
-  const periodInDays = Math.floor(periodInMillis / (1000 * 60 * 60 * 24));
+bookingSchema.pre("save", async function () {
+  const periodInMs = this.dateTo - this.dateFrom;
+  const periodInDays = Math.floor(periodInMs / (1000 * 60 * 60 * 24));
 
-  if (periodInDays < 0) {
-    return next(new Error("End date must be after start date!"));
+  if (periodInDays < 1) {
+    throw new Error("Period must be at least 1 day!");
   }
 
-  this.model("Property").findById(this.property, (err, property) => {
-    if (err || !property) {
-      return next(new Error("Property not found or error fetching property!"));
-    }
-    this.bill = periodInDays * property.pricePerNight;
-    this.period = daysDiffTime;
-    next();
-  });
+  const property = await this.model("Property").findById(this.property);
+  if (!property) {
+    throw new Error("Property not found!");
+  }
+
+  this.bill = periodInDays * property.pricePerNight;
+  this.period = periodInDays;
 });
 
 const Booking = model("Booking", bookingSchema);
