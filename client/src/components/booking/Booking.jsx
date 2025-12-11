a9f42cb57b13453172c5d0ab607ad97364716274
Руслан Каписъски <ruslank1@abv.bg>
2025-12-11 13:14:04 +0200

    Fix project structure

--- client/src/components/booking/Booking.jsx
+++ client/src/components/booking/Booking.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
-import useForm from "../hooks/useForm";
-import useApi from "../hooks/useApi";
-import { useUserContext } from "../contexts/UserContext";
+import useForm from "../../hooks/useForm";
+import useApi from "../../hooks/useApi";
+import { useUserContext } from "../../contexts/UserContext";
 
 export default function Booking({ property }) {
   const [startDate, setStartDate] = useState(new Date());
